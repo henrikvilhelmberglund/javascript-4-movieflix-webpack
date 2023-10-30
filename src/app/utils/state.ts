52d340431cc41d59a0cd9d30b83a57459a51e863
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-30 15:56:47 +0100

    feat: convert state

--- src/app/utils/state.ts
+++ src/app/utils/state.ts
@@ -1,6 +1,13 @@
-export const state = {
+interface ApiInterface {
   api: {
-    key: 'cfd8ad8f2d19b724a21ca2510b450da9',
-    baseUrl: 'https://api.themoviedb.org/3/',
+    key: string;
+    baseUrl: string;
+  };
+}
+
+export const state: ApiInterface = {
+  api: {
+    key: "cfd8ad8f2d19b724a21ca2510b450da9",
+    baseUrl: "https://api.themoviedb.org/3/",
   },
 };
