35fd25ee49e16961d69b2e50f0557411cd8d1460
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-30 15:57:11 +0100

    feat: add Movie interface and Genre type

---
+++ src/types/movie.ts
@@ -0,0 +1,16 @@
+interface Movie {
+  poster_path: string;
+  title: string;
+  release_date: string;
+  id: number;
+  vote_average: number;
+  overview: string;
+  genres: Genre[];
+  runtime: number;
+  backdrop_path: string;
+}
+
+type Genre = {
+  id: number;
+  name: string;
+};
