76d39297dee15ffa0a7d446420f1942cb1884ec1
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-01 16:57:55 +0100

    fix: comment out old client fetch

--- src/app/utils/https.ts
+++ src/app/utils/https.ts
@@ -1,16 +1,16 @@
-import { state } from './state';
+// import { state } from './state';
 
-export const fetchData = async (endpoint: string, page = 1, criteria: string | undefined = undefined) => {
-  const API_KEY = state.api.key;
-  const API_URL = state.api.baseUrl;
+// export const fetchData = async (endpoint: string, page = 1, criteria: string | undefined = undefined) => {
+//   const API_KEY = state.api.key;
+//   const API_URL = state.api.baseUrl;
 
-  let response;
-  if (criteria) {
-    response = await fetch(`${API_URL}${endpoint}?query=${criteria}&api_key=${API_KEY}&language=sv-SE&page=${page}`);
-  } else {
-    response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=sv-SE&page=${page}`);
-  }
+//   let response;
+//   if (criteria) {
+//     response = await fetch(`${API_URL}${endpoint}?query=${criteria}&api_key=${API_KEY}&language=sv-SE&page=${page}`);
+//   } else {
+//     response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=sv-SE&page=${page}`);
+//   }
 
-  const data = await response.json();
-  return data;
-};
+//   const data = await response.json();
+//   return data;
+// };
